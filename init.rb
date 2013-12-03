5d434c528002172cdecf3e09aa91bd47f1ca947f
Eddie Roger <eroger@exacttarget.com>
2013-12-03 18:10:15 -0500

    Cleaned up external project

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ require 'bundler/setup'
 APP_ROOT = File.dirname(__FILE__)
 
 # Add gems that you want loaded here, space delimited
-%w{ sinatra/base sinatra/content_for sinatra/config_file sinatra/respond_with rack-flash rack/contrib rack/csrf warden data_mapper dm-serializer base64 openssl httparty zlib net/http net/https}.each {|req| require req }
+%w{ sinatra/base sinatra/content_for sinatra/config_file sinatra/respond_with rack-flash rack/contrib rack/csrf warden data_mapper dm-serializer base64 zlib }.each {|req| require req }
 
 # Load up your custom libs
 Dir["lib/*.rb"].sort.each {|req| require_relative req }
